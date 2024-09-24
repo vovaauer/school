ad4764163d4f1a84cdb9296b9e0a438e1f52f2ef
iAmAlbert <57306890+i-am-albert@users.noreply.github.com>
2024-09-24 13:44:38 -0600

    Update sketch.js

--- interactivescene/sketch.js
+++ interactivescene/sketch.js
@@ -12,6 +12,8 @@ let tx
 let ty
 let tr
 let music
+let place
+let tplace
 
 function preload(){
   music = loadSound("music.mp3")
@@ -25,11 +27,13 @@ function setup() {
   r = height/3
   x = width/2-r/1.35
   y = height/2-r/6
+  place = 0
   tx = x
   ty = y
   tr = r
+  tplace = 0
   music.play();
-  music.rate(0)
+  music.rate(0);
 }
 
 function draw() {
@@ -37,16 +41,19 @@ function draw() {
   x+=(tx-x)*0.07
   y+=(ty-y)*0.07
   r+=(tr-r)*0.07
-  music.rate((tr-r)/100)
+  place+=(tplace-place)*0.1
+  music.rate((tplace-place)/10)
   image(man,x,y,r*1.5,r);
   if (keyIsDown(UP_ARROW)) {
     if (tr>height/3) {
+      tplace-=1
       tx-=-width/17.5/400*r/100
       ty-=-width/50/400*r/100
       tr-=width/10/400*r/100
     }
   }
   if (keyIsDown(DOWN_ARROW)) {
+    tplace+=1
     tx+=-width/17.5/400*r/100
     ty+=-width/50/400*r/100
     tr+=width/10/400*r/100
@@ -56,12 +63,14 @@ function draw() {
 function mouseWheel(event) {
   if (tr>height/3) {
     if (event.delta < 0) {
+      tplace-=1
       tx-=-width/17.5
       ty-=-width/50
       tr-=width/10
     }
   }
   if (event.delta > 0) {
+    tplace+=1
     tx+=-width/17.5
     ty+=-width/50
     tr+=width/10
