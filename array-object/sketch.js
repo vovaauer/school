210db2049ff05e5095f4b90812df95d0c14947bd
Auer0403800 <0403800@spsd.sk.ca>
2024-10-09 14:16:04 -0600

    Worked on array object project to add players

--- array-object/sketch.js
+++ array-object/sketch.js
@@ -5,28 +5,37 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let player = {x: 0, y: 0, color: "red"};
+let playerid;
 let players = [];
+let player;
 
 function preload() {
   // connect to a p5party server
   partyConnect("wss://demoserver.p5party.org", "albert");
   
   // tell p5.party to sync the pos object
-  pos = partyLoadShared("pos", pos);
-  
+  player = partyLoadShared("player", player);
 }
 
 function setup() {
+  playerid = Math.floor(Math.random() * 100);
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   background(255);
-  pos.x = mouseX;
-  pos.y = mouseY;
-  for (player of players) {
-    ellipse(player.x, player.y, 100, 100);
+  let player = {
+    x: mouseX,
+    y: mouseY,
+    id: playerid,
+  };
+  players.push(player);
+  for (let allplayers of players) {
+    circle(allplayers.x,allplayers.y,playerid);
   }
+  console.log(players);
+  // for (allplayers in players) {
+  //   circle(allplayers.x,allplayers.y,10);
+  // }
 }
 
