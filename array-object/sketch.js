c037efa9d3a0b72f6084f712b3a7f8a590d739f5
Auer0403800 <0403800@spsd.sk.ca>
2024-10-11 11:30:54 -0600

    players can now see each other's cursors

--- array-object/sketch.js
+++ array-object/sketch.js
@@ -8,13 +8,15 @@
 let playerid;
 let players;
 let player;
+let guests;
 
 function preload() {
   // connect to a p5party server
   partyConnect("wss://demoserver.p5party.org", "albert");
-  shared = partyLoadShared("main");
-  players = partyLoadGuestShareds();
+  // shared = partyLoadShared("main");
+  guests = partyLoadGuestShareds();
   me = partyLoadMyShared();
+  cursor = loadImage('cursor.svg');
 }
 
 function setup() {
@@ -24,18 +26,26 @@ function setup() {
 
 function draw() {
   background(255);
-  let player = {
-    x: mouseX,
-    y: mouseY,
-    id: playerid,
-  };
-  players.push(player);
-  for (let allplayers of players) {
-    circle(allplayers.x,allplayers.y,playerid);
+  for (let i = 0; i < guests.length; i++) {
+    const p = guests[i];
+    image(cursor,p.x,p.y,32,32);
   }
-  console.log(players);
+  me.x = mouseX;
+  me.y = mouseY;
+  // let player = {
+  //   x: mouseX,
+  //   y: mouseY,
+  //   id: playerid,
+  // };
+  // players.push(player);
+  // for (let allplayers of players) {
+  //   circle(allplayers.x,allplayers.y,playerid);
+  // }
+  // console.log(players);
   // for (allplayers in players) {
   //   circle(allplayers.x,allplayers.y,10);
   // }
 }
 
+function mouseClicked() {
+}
