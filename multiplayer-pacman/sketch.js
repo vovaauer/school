3bfd4f3c6553f1a9fd1f39ea1a214ca1dce4db75
iAmAlbert <57306890+i-am-albert@users.noreply.github.com>
2024-10-28 14:15:43 -0600

    added p5.party

--- multiplayer-pacman/sketch.js
+++ multiplayer-pacman/sketch.js
@@ -5,11 +5,15 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+function preload() {
+  partyConnect("wss://demoserver.p5party.org", "albertpacman");
+  shared = partyLoadShared("main");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(220);
+  background(0);
 }
