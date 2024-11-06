5dcc98e5d720924d6cf36d74851a2057a93341d5
iAmAlbert <57306890+i-am-albert@users.noreply.github.com>
2024-11-06 14:17:06 -0600

    started on state variables (start screen) , changed how grid will be done by using an image instead of drawing the map, added some images

--- multiplayer-pacman/sketch.js
+++ multiplayer-pacman/sketch.js
@@ -5,11 +5,16 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-const GRID_SIZE = 20;
+const GRID_SIZE = 32;
 
 let shared;
 let character = {};
 let lines;
+let pacmap;
+let pacman;
+let shadow;
+let speedy;
+let started = false;
 
 function preload() {
   partyConnect("wss://demoserver.p5party.org", "albertpacman");
@@ -17,55 +22,66 @@ function preload() {
   me = partyLoadMyShared();
   guests = partyLoadGuestShareds();
   lines = loadStrings("1.txt");
+  pacmap = loadImage("pacmap.png");
+  pacman = loadImage("pacman_2.png");
+  shadow = loadImage("shadow_right.png");
+  speedy = loadImage("speedy_right.png");
 }
 
 function setup() {
   noSmooth();
   noStroke();
-  createCanvas(windowWidth, windowHeight); 
+  textSize(32);
+  fill(255);
+  stroke(0);
+  strokeWeight(4);
+  textAlign(CENTER);
+  createCanvas(560, 620); 
   if (partyIsHost()) {
     generateGrid(GRID_SIZE,GRID_SIZE);
   }
 }
 
 function generateGrid(cols,rows) {
-  shared.grid=[];
-  for (let y=0;y<cols;y++) {
-    shared.grid.push([]);
-    for (let x=0;x<rows;x++) {
-      shared.grid.push(lines[y][x]);
-    }
-  }
+  shared.grid=lines;
 }
-
-function checkNeighbours(y,x) {
-  let neighbours = 0;
-  if (shared.grid[y-1][x-1]==="wall") {
-    neighbours+=1;
-  }
-  return neighbours>1;
-}
-
 
 function draw() {
-  displayGrid();
+  if (!started) {
+    chooseCharacter();
+  }
+  if (started) {
+    displayGrid();
+  }
+}
+
+function chooseCharacter() {
+  let pacmanExists = false;
+  background(0);
+  for (let i = 0; i < guests.length; i++) {
+    // display players
+    const p = guests[i];
+    if (p.character==="pacman") {
+      pacmanExists = true;
+    }
+  }
+  if (pacmanExists) {
+    text('You are pacman! Click him to enter.',width/2,height/2);
+    image(pacman,width/2,height/2);
+  }
+  else {
+    text('You are a ghost! Click a ghost to start.',width/2,height/2);
+    image(shadow,width/2,height/2);
+  }
 }
 
 function displayGrid() {
-  background(0);
+  image(pacmap,0,0,width,height);
   for (let y=0;y<GRID_SIZE;y++) {
     for (let x=0;x<GRID_SIZE;x++) {
-      if (shared.grid[y][x] === "wall") {
+      if (shared.grid[y][x] === "P") {
         fill("blue");
         square(height/GRID_SIZE*x,height/GRID_SIZE*y,height/GRID_SIZE);
-      }
-      else if (shared.grid[y][x] === "food") {
-        fill("white");
-        circle(height/GRID_SIZE*x+GRID_SIZE,height/GRID_SIZE*y+GRID_SIZE,GRID_SIZE/2);
-      }
-      else if (shared.grid[y][x] === "power") {
-        fill("white");
-        circle(height/GRID_SIZE*x+GRID_SIZE,height/GRID_SIZE*y+GRID_SIZE,GRID_SIZE);
       }
     }
   }
